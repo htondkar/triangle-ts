ed2e42061fde93063636c96e9275b5f3ea5158cf
Hamidreza <htondkar@gmail.com>
2019-05-27 22:49:29 +0430

    implement trinagle result component

--- src/pages/TriangleType/TriangleTypeResult.tsx
+++ src/pages/TriangleType/TriangleTypeResult.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
 
-export const TriangleTypeResult = () => {
-  return <div style={{ background: 'white' }}>REsults !</div>
+import { EquilateralTriangle } from './components/EquilateralTriangle';
+import { IsoclinesTriangle } from './components/IsoclinesTriangle';
+import { ScaleneTriangle } from './components/ScelaneTriangle';
+import { determineTriangleType, isValidTriangle, Triangle, TriangleTypes } from './Triangle.model';
+
+interface Props {
+  triangle: Partial<Triangle> | null
+}
+
+const messages: Record<TriangleTypes, React.ReactNode> = {
+  [TriangleTypes.Equilateral]: (
+    <div>
+      It is Equilateral <EquilateralTriangle />
+    </div>
+  ),
+  [TriangleTypes.Isosceles]: (
+    <div>
+      It is Isosceles <IsoclinesTriangle />
+    </div>
+  ),
+  [TriangleTypes.Scalene]: (
+    <div>
+      It is Scalene <ScaleneTriangle />
+    </div>
+  ),
+}
+
+export const TriangleTypeResult: React.FunctionComponent<Props> = ({ triangle }) => {
+  if (triangle === null) {
+    return <div>Please fill all the fields</div>
+  }
+
+  if (!isValidTriangle(triangle)) {
+    return <div>This is not a valid triangle</div>
+  }
+
+  const triangleType = determineTriangleType(triangle)
+
+  return <div style={{ background: 'white' }}>{messages[triangleType]}</div>
 }
