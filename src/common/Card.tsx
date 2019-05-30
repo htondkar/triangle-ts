27a923365569c2266e1efd2e88cf5532f0c938f2
Hamidreza <htondkar@gmail.com>
2019-05-30 14:59:41 +0430

    add card component

---
+++ src/common/Card.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Card = styled.section`
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  background-color: white;
+`
