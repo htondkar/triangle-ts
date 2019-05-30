0f28dcce50e90b5c247fc1eba4dab663c7b66dd2
Hamidreza <htondkar@gmail.com>
2019-05-30 15:01:39 +0430

    minor fix in svg path of IsoclinesTriangle

--- src/pages/TriangleType/components/IsoclinesTriangle.tsx
+++ src/pages/TriangleType/components/IsoclinesTriangle.tsx
@@ -5,7 +5,7 @@ import { TriangleSvg } from './TriangleSvg';
 export const IsoclinesTriangle = () => (
   <TriangleSvg>
     <g>
-      <path d="M 50 0 L 85 100 L 15 100 Z" />
+      <path d="M 50 0 L 85 95 L 15 95 Z" />
     </g>
   </TriangleSvg>
 )
