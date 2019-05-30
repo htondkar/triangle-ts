1846cdd7dd9f3ef36b8311752dc9827bcda3d210
Hamidreza <htondkar@gmail.com>
2019-05-30 15:27:34 +0430

    add a comment to index.tsx to make thing more clear

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import { TradehshiftUi } from './types/tradeshift-ui';
 
+// define ts as a global variable
 declare var ts: TradehshiftUi
 
 function renderApp() {
