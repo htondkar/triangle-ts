0439f7cc96cea27d32f09a6e42b7137b795c892e
Hamidreza <htondkar@gmail.com>
2019-05-30 15:01:05 +0430

    improve results component styles and code formatting

--- src/pages/TriangleType/TriangleTypeResult.tsx
+++ src/pages/TriangleType/TriangleTypeResult.tsx
@@ -1,42 +1,65 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import { Card } from '../../common/Card';
 import { EquilateralTriangle } from './components/EquilateralTriangle';
 import { IsoclinesTriangle } from './components/IsoclinesTriangle';
-import { ScaleneTriangle } from './components/ScelaneTriangle';
-import { determineTriangleType, isValidTriangle, Triangle, TriangleTypes } from './Triangle.model';
+import { ScaleneTriangle } from './components/ScaleneTriangle';
+import { Triangle, TriangleTypes } from './Triangle.model';
+import { determineTriangleType } from './TriangleType.utils';
+import { TriangleValidations } from './TriangleType.validations';
 
 interface Props {
   triangle: Partial<Triangle> | null
 }
 
-const messages: Record<TriangleTypes, React.ReactNode> = {
+const ResultCard = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  height: 250px;
+  overflow: auto;
+`
+
+const messages = {
   [TriangleTypes.Equilateral]: (
-    <div>
-      It is Equilateral <EquilateralTriangle />
-    </div>
+    <React.Fragment>
+      <span>
+        <strong>Equilateral</strong> Triangle!
+      </span>
+      <EquilateralTriangle />
+    </React.Fragment>
   ),
   [TriangleTypes.Isosceles]: (
-    <div>
-      It is Isosceles <IsoclinesTriangle />
-    </div>
+    <React.Fragment>
+      <span>
+        <strong>Isosceles</strong> Triangle!
+      </span>
+      <IsoclinesTriangle />
+    </React.Fragment>
   ),
   [TriangleTypes.Scalene]: (
-    <div>
-      It is Scalene <ScaleneTriangle />
-    </div>
+    <React.Fragment>
+      <span>
+        <strong>Scalene </strong> Triangle!
+      </span>
+      <ScaleneTriangle />
+    </React.Fragment>
   ),
+  invalidInput: <React.Fragment>This is not a valid triangle</React.Fragment>,
+  incompleteInput: <React.Fragment>Please fill all the fields</React.Fragment>,
 }
 
 export const TriangleTypeResult: React.FunctionComponent<Props> = ({ triangle }) => {
-  if (triangle === null) {
-    return <div>Please fill all the fields</div>
+  if (triangle === null || !TriangleValidations.hasAllTheSides(triangle)) {
+    return <ResultCard>{messages.incompleteInput}</ResultCard>
   }
 
-  if (!isValidTriangle(triangle)) {
-    return <div>This is not a valid triangle</div>
+  if (!TriangleValidations.passesTriangleInequalityTheoremTest(triangle)) {
+    return <ResultCard>{messages.invalidInput}</ResultCard>
   }
 
-  const triangleType = determineTriangleType(triangle)
-
-  return <div style={{ background: 'white' }}>{messages[triangleType]}</div>
+  return <ResultCard>{messages[determineTriangleType(triangle)]}</ResultCard>
 }
